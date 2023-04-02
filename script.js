35686a6e6e768536c6d5b4b43ed2eaa21e2a11a7
Ackim Brian <kimbrian2010@gmail.com>
2023-04-02 22:09:54 +0200

    Save notes to local storage

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const btn = document.getElementById("btn");
 const containerEl = document.getElementById("container");
 
+getNotes().forEach((note) => {
+    const noteEl = createNoteEl(note.id, note.content);
+    containerEl.insertBefore(noteEl, btn);
+});
+
 function createNoteEl(id, content) {
     const element = document.createElement("textarea");
     element.classList.add("note");
@@ -16,14 +21,22 @@ function createNoteEl(id, content) {
     })
 
     element.addEventListener("input", () => {
+            updateNote(id, element.value);
 
         })
         // console.log(id, content)
     return element;
 }
 
-function updateNote() {
+function updateNote(id, content) {
+    const notes = getNotes();
+    const target = notes.filter((note) => note.id == id)[0];
+    target.content = content;
+    saveNote(notes);
 
+    function saveNote() {
+        localStorage.setItem("note-app", JSON.stringify(notes));
+    }
 }
 
 function deleteNote() {
@@ -31,6 +44,7 @@ function deleteNote() {
 }
 
 function addNote() {
+    const notes = getNotes();
     const noteObject = {
         id: Math.floor(Math.random() * 100000),
         content: "",
@@ -39,6 +53,20 @@ function addNote() {
 
     const noteEl = createNoteEl(noteObject.id, noteObject.content);
     containerEl.insertBefore(noteEl, btn);
+
+    notes.push(noteObject);
+    saveNote(notes);
+
+    function saveNote() {
+        localStorage.setItem("note-app", JSON.stringify(notes));
+    }
+
+}
+
+
+
+function getNotes() {
+    return JSON.parse(localStorage.getItem("note-app") || "[]")
 }
 
 btn.addEventListener("click", addNote);
